4add0c25311ff891a18799a89903bfa117dd6b85
Glenn Dixon <donblanco@posteo.net>
2020-08-24 13:28:59 -0700

    comment out src line

--- js/picture.js
+++ js/picture.js
@@ -1,10 +1,10 @@
 
 module.exports = (url, alt = "Missing alt text") => {
     return `<picture>
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_320/gtd/${url}" media="(max-width: 320px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_375/gtd/${url}" media="(max-width: 375px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_414/gtd/${url}" media="(max-width: 414px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" media="(min-width: 755px)">
-    <img src="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" alt="${alt}" />
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_320/gtd/${url}" alt="${alt}" media="(max-width: 320px)">
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_375/gtd/${url}" alt="${alt}" media="(max-width: 375px)">
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_414/gtd/${url}" alt="${alt}" media="(max-width: 414px)">
+    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" alt="${alt}" media="(min-width: 755px)">
+// <img src="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" alt="${alt}" />
   </picture>`;
   };
