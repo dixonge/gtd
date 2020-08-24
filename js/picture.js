703626dbf0a5219fe253f4c2554dfa13f6f14383
Glenn Dixon <donblanco@posteo.net>
2020-08-24 13:35:15 -0700

    remove src line

--- js/picture.js
+++ js/picture.js
@@ -5,6 +5,5 @@ module.exports = (url, alt = "Missing alt text") => {
     <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_375/gtd/${url}" alt="${alt}" media="(max-width: 375px)">
     <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_414/gtd/${url}" alt="${alt}" media="(max-width: 414px)">
     <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" alt="${alt}" media="(min-width: 755px)">
-// <img src="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" alt="${alt}" />
   </picture>`;
   };
