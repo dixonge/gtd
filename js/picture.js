3872401c249afa11b9fbfb9dbe3754051c6688fe
Glenn Dixon <donblanco@posteo.net>
2020-08-24 13:43:38 -0700

    alternate syntax for srcset

--- js/picture.js
+++ js/picture.js
@@ -1,9 +1,7 @@
 
 module.exports = (url, alt = "Missing alt text") => {
-    return `<picture>
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_320/gtd/${url}" alt="${alt}" media="(max-width: 320px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_375/gtd/${url}" alt="${alt}" media="(max-width: 375px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_414/gtd/${url}" alt="${alt}" media="(max-width: 414px)">
-    <source srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" alt="${alt}" media="(min-width: 755px)">
-  </picture>`;
+    return `
+ <img src="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url}" alt="${alt}
+ srcset="https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_320/gtd/${url} 320w, https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_375/gtd/${url} 375w, https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_414/gtd/${url} 414w, https://res.cloudinary.com/donblanco/image/upload/f_auto,q_auto,w_756/gtd/${url} 755w" />
+ `;
   };
